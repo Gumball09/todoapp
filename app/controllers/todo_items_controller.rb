3a06530289a1e8edd1dd5defc5196194b6865592
Shubh <shubh.mehrotra@scaler.com>
2026-01-06 18:25:59 +0530

    feat : changes application.css -> application.scss

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,47 +1,37 @@
 class TodoItemsController < ApplicationController
- before_action :set_todo_list
- 
-def create
- @todo_item = @todo_list.todo_items.create(todo_item_params)
- redirect_to @todo_list
+  before_action :set_todo_list
+  before_action :set_todo_item, only: [:destroy, :complete]
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  def destroy
+    if @todo_item.destroy
+      flash[:success] = "Todo item was deleted."
+    else
+      flash[:error] = "Todo item could not be deleted."
+    end
+    redirect_to @todo_list
+  end
+
+  def complete
+    @todo_item.update(completed_at: Time.current)
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def todo_item_params
+    params.require(:todo_item).permit(:content)
+  end
 end
-
-def destroy
-  @todo_item = @todo_list.todo_items.find(params[:id])
- if @todo_item.destroy
-  flash[:success] = "Todo List item was deleted."
- else
-  flash[:error] = "Todo List item could not be deleted."
- end
- redirect_to @todo_list 
-end
-
-def complete
- @todo_item.update_attribute(:completed_at, Time.now)
- redirect_to @todo_list, notice: "Todo item completed"
-end
-
-private
-
-def set_todo_list
- @todo_list = TodoList.find(params[:todo_list_id])
-end
-
-def todo_item_params
- params[:todo_item].permit(:content)
-end
-
-def set_todo_item
-  @todo_item = @todo_list.todo_items.find(params[:id])
-end
-
-def todo_item_params
- params[:todo_item].permit(:content)
-end
-
-# we have added a private method, a complete action and the before action.
-
-end
-
-# We have created an action for creating new todo(s) 
-# Ab 2 Partials banayenge. 
