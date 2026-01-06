de9fd91a9a6c09059d9618766bb154f1728ea686
Shubh <shubh.mehrotra@scaler.com>
2026-01-06 18:00:47 +0530

    feat : complete todo method created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items #this tells Rails, that Todo Items belong to a Todo List
+    member do
+      patch :complete
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
