ece9cfb620aa6ed01da25165e7d2627c76d6febc
Shubh <shubh.mehrotra@scaler.com>
2026-01-06 17:00:57 +0530

    feat: create routes, model and a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items #this tells Rails, that Todo Items belong to a Todo List
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'todo_lists#index'
+
 end
